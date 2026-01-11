0d3c0e5ed3b5863e7f95bd7ff0276dd71bddcd80
Kilson <96642091+kenstudenthk@users.noreply.github.com>
2026-01-11 18:59:29 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // 這行最重要，它會掃描你的 React 組件
+    "./*.{js,ts,jsx,tsx}",        // 掃描根目錄下的 App.tsx, main.tsx 等
+    "./components/**/*.{js,ts,jsx,tsx}", // 掃描 components 資料夾
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#0d9488',
+        secondary: '#fb7185',
+      }
+    },
   },
   plugins: [],
 }

e006b52bf7c2bb66c35f71d5f9597b2dad6a0d98
Kilson <96642091+kenstudenthk@users.noreply.github.com>
2026-01-11 17:43:07 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -23,4 +23,5 @@ export const SP_FIELDS = {
   PHONE: 'field_37',
   CONTACT: 'field_38',
   REMARK: 'Remark' // 根據你的資料，Remark 是唯一一個直接用名稱的
+  SCHEDULE_GROUP: 'Schedule_x0020_Group' // ✅ Add this for Group mapping
 };

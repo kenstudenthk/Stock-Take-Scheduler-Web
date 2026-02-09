aa1d6c92d3229408aaecd76d153939e20d06327e
kenstudenthk <kenstudenthk@yahoo.com.hk>
2026-02-09 17:15:07 +0800

    fix: Remove export of deleted GroupSelector to fix build

--- components/MobileMapView/index.ts
+++ components/MobileMapView/index.ts
@@ -1,5 +1,4 @@
 export { MobileMapView } from './MobileMapView';
-export { GroupSelector } from './GroupSelector';
 export { BottomSheet } from './BottomSheet';
 export type { BottomSheetState } from './BottomSheet';
 export { RoutePanel } from './RoutePanel';
